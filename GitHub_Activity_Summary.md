af0d95d48dad77536489f35b06d92f5004768ccc
jayantnehra55 <jayant.nehra@fiftyfivetech.io>
2023-08-28 18:14:30 +0530

    Feat: added support for the Markdown format for the digest

---
+++ GitHub_Activity_Summary.md
@@ -0,0 +1,112 @@
+# GitHub Activity Summary
+
+
+## 1. Repository: Bio-Informatics
+### Branch: main
+#### Recent Commits
+1. No activity in the last 7 days.
+#### Recent Comments
+1. No activity in the last 7 days.
+#### Recent Pull Requests
+1. No activity in the last 7 days.
+#### Recent Issues
+1. No activity in the last 7 days.
+
+## 2. Repository: CIFAR
+### Branch: main
+#### Recent Commits
+1. No activity in the last 7 days.
+#### Recent Comments
+1. No activity in the last 7 days.
+#### Recent Pull Requests
+1. No activity in the last 7 days.
+#### Recent Issues
+1. No activity in the last 7 days.
+
+## 3. Repository: desktop-tutorial
+### Branch: first
+#### Recent Commits
+1. No activity in the last 7 days.
+#### Recent Comments
+1. No activity in the last 7 days.
+#### Recent Pull Requests
+1. No activity in the last 7 days.
+#### Recent Issues
+1. No activity in the last 7 days.
+
+## 3. Repository: desktop-tutorial
+### Branch: master
+#### Recent Commits
+1. No activity in the last 7 days.
+#### Recent Comments
+1. No activity in the last 7 days.
+#### Recent Pull Requests
+1. No activity in the last 7 days.
+#### Recent Issues
+1. No activity in the last 7 days.
+
+## 4. Repository: Github_Activity_Summary
+### Branch: main
+#### Recent Commits
+1. Update github_activity.sh 2. First Commit 3. Automate the process of generating a daily activity digest from GitHub repositories 4. Initial commit
+#### Recent Comments
+1. No activity in the last 7 days.
+#### Recent Pull Requests
+1. No activity in the last 7 days.
+#### Recent Issues
+1. No activity in the last 7 days.
+
+## 5. Repository: Identification
+### Branch: main
+#### Recent Commits
+1. No activity in the last 7 days.
+#### Recent Comments
+1. No activity in the last 7 days.
+#### Recent Pull Requests
+1. No activity in the last 7 days.
+#### Recent Issues
+1. No activity in the last 7 days.
+
+## 6. Repository: Object-Detection
+### Branch: main
+#### Recent Commits
+1. No activity in the last 7 days.
+#### Recent Comments
+1. No activity in the last 7 days.
+#### Recent Pull Requests
+1. No activity in the last 7 days.
+#### Recent Issues
+1. No activity in the last 7 days.
+
+## 7. Repository: quiz
+### Branch: master
+#### Recent Commits
+1. No activity in the last 7 days.
+#### Recent Comments
+1. No activity in the last 7 days.
+#### Recent Pull Requests
+1. No activity in the last 7 days.
+#### Recent Issues
+1. No activity in the last 7 days.
+
+## 7. Repository: quiz
+### Branch: solution-p1
+#### Recent Commits
+1. No activity in the last 7 days.
+#### Recent Comments
+1. No activity in the last 7 days.
+#### Recent Pull Requests
+1. No activity in the last 7 days.
+#### Recent Issues
+1. No activity in the last 7 days.
+
+## 7. Repository: quiz
+### Branch: solution-p2
+#### Recent Commits
+1. No activity in the last 7 days.
+#### Recent Comments
+1. No activity in the last 7 days.
+#### Recent Pull Requests
+1. No activity in the last 7 days.
+#### Recent Issues
+1. No activity in the last 7 days.
--- github_activity.sh
+++ github_activity.sh
@@ -1,82 +1,74 @@
 #!/bin/bash
 
-# Set your GitHub username and personal access token
-GITHUB_USERNAME="github_username"
-ACCESS_TOKEN="github_access_token"
+# Set your GitHub username and Personal Access Token
+GITHUB_USERNAME="Jay-Nehra"
+GITHUB_TOKEN="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"
 
-# Set the date for which you want to generate the summary
-DATE=$(date +'%Y-%m-%d')
+# Initialize the date one week ago
+ONE_WEEK_AGO=$(date -d '7 days ago' +%Y-%m-%dT%H:%M:%SZ)
 
-# Define a function to fetch GitHub activity using GraphQL API
-get_github_activity() {
-    local query='
-    {
-        user(login: "'$GITHUB_USERNAME'") {
-            contributionsCollection(from: "'$DATE'", to: "'$DATE'") {
-                pullRequestContributionsByRepository {
-                    repository {
-                        name
-                    }
-                    contributions(first: 10) {
-                        nodes {
-                            pullRequest {
-                                title
-                                url
-                            }
-                        }
-                    }
-                }
-                issueContributionsByRepository {
-                    repository {
-                        name
-                    }
-                    contributions(first: 10) {
-                        nodes {
-                            issue {
-                                title
-                                url
-                            }
-                        }
-                    }
-                }
-                commitContributionsByRepository {
-                    repository {
-                        name
-                    }
-                    contributions(first: 10) {
-                        nodes {
-                            commit {
-                                message
-                                url
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    '
+# Initialize an empty string to store the activity summary
+ACTIVITY_SUMMARY="# GitHub Activity Summary\n"
 
-    curl -s -H "Authorization: Bearer $ACCESS_TOKEN" -X POST -d "{'query': '$query'}" https://api.github.com/graphql
-}
+# Initialize repository count
+REPO_COUNT=1
 
-# Call the function to fetch GitHub activity
-activity_response=$(get_github_activity)
+# Fetch list of repositories for the user
+REPO_LIST=$(curl -s -u $GITHUB_USERNAME:$GITHUB_TOKEN https://api.github.com/user/repos | jq -r '.[].name')
 
-# Parse the JSON response to extract relevant information
-pull_requests=$(echo "$activity_response" | jq -r '.data.user.contributionsCollection.pullRequestContributionsByRepository')
-issues=$(echo "$activity_response" | jq -r '.data.user.contributionsCollection.issueContributionsByRepository')
-commits=$(echo "$activity_response" | jq -r '.data.user.contributionsCollection.commitContributionsByRepository')
+# Loop through each repository to get activities
+for repo in $REPO_LIST; do
+    # Initialize strings for each activity type
+    RECENT_COMMITS=""
+    RECENT_COMMENTS=""
+    RECENT_PULLS=""
+    RECENT_ISSUES=""
 
-# Print the summary
-echo "GitHub Activity Summary for $DATE"
-echo "==================================="
+    # Fetch list of branches for the repository
+    BRANCH_LIST=$(curl -s -u $GITHUB_USERNAME:$GITHUB_TOKEN https://api.github.com/repos/$GITHUB_USERNAME/$repo/branches | jq -r '.[].name')
 
-echo "Pull Requests:"
-echo "$pull_requests" | jq -r '.[] | "- " + .repository.name + ": " + .contributions.nodes[].pullRequest.title + " (" + .contributions.nodes[].pullRequest.url + ")"'
+    for branch in $BRANCH_LIST; do
+        # Fetch recent commits for the repository from the last week
+        COMMITS_RESPONSE=$(curl -s -u $GITHUB_USERNAME:$GITHUB_TOKEN "https://api.github.com/repos/$GITHUB_USERNAME/$repo/commits?since=$ONE_WEEK_AGO&sha=$branch")
+        COMMIT_MESSAGES=$(echo "$COMMITS_RESPONSE" | jq -r '.[].commit.message')
 
-echo "Issues:"
-echo "$issues" | jq -r '.[] | "- " + .repository.name + ": " + .contributions.nodes[].issue.title + " (" + .contributions.nodes[].issue.url + ")"'
+        if [ -z "$COMMIT_MESSAGES" ]; then
+            RECENT_COMMITS="1. No activity in the last 7 days."
+        else
+            RECENT_COMMITS=$(echo "$COMMIT_MESSAGES" | nl -s '. ' -w 1)
+        fi
 
-echo "Commits:"
-echo "$commits" | jq -r '.[] | "- " + .repository.name + ": " + .contributions.nodes[].commit.message + " (" + .contributions.nodes[].commit.url + ")"'
+        # Fetch open pull requests
+        PULLS=$(curl -s -u $GITHUB_USERNAME:$GITHUB_TOKEN https://api.github.com/repos/$GITHUB_USERNAME/$repo/pulls | jq -r '.[].title')
+        if [ -z "$PULLS" ]; then
+            RECENT_PULLS="1. No activity in the last 7 days."
+        else
+            RECENT_PULLS=$(echo "$PULLS" | nl -s '. ' -w 1)
+        fi
+
+        # Fetch open issues
+        ISSUES=$(curl -s -u $GITHUB_USERNAME:$GITHUB_TOKEN https://api.github.com/repos/$GITHUB_USERNAME/$repo/issues | jq -r '.[].title')
+        if [ -z "$ISSUES" ]; then
+            RECENT_ISSUES="1. No activity in the last 7 days."
+        else
+            RECENT_ISSUES=$(echo "$ISSUES" | nl -s '. ' -w 1)
+        fi
+
+        # Fetch code comments
+        COMMENTS=$(curl -s -u $GITHUB_USERNAME:$GITHUB_TOKEN https://api.github.com/repos/$GITHUB_USERNAME/$repo/commits/$branch/comments | jq -r '.[].body')
+        if [ -z "$COMMENTS" ]; then
+            RECENT_COMMENTS="1. No activity in the last 7 days."
+        else
+            RECENT_COMMENTS=$(echo "$COMMENTS" | nl -s '. ' -w 1)
+        fi
+
+        # Add the activities to the activity summary in the specified format
+        ACTIVITY_SUMMARY+="\n\n## $REPO_COUNT. Repository: $repo\n### Branch: $branch\n#### Recent Commits\n$RECENT_COMMITS\n#### Recent Comments\n$RECENT_COMMENTS\n#### Recent Pull Requests\n$RECENT_PULLS\n#### Recent Issues\n$RECENT_ISSUES"
+    done
+
+    # Increment repository count
+    REPO_COUNT=$((REPO_COUNT + 1))
+done
+
+# Print the activity summary or save it to a file
+echo -e $ACTIVITY_SUMMARY > GitHub_Activity_Summary.md
